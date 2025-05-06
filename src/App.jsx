35fc74bc13b2db1befc96e2a9a3c5f69a593f03e
Pedro Henrique Budziak <84034586+pedroph7@users.noreply.github.com>
2025-05-06 17:03:28 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState,useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+
+    const [cep, setCep] = useState("80510-070");
+    const [bairro, setBairro] = useState("");
+    const [rua, setRua] = useState("");
+    const [estado, setEstado] = useState("");
+    const [localidade, setLocalidade] = useState("");
+  
+  
+    function handleCep(e) {
+      setCep(e.target.value);
+    }
+  
+    useEffect(() => {
+      const sanitizedCep = cep.replace(/\D/g, "");
+  
+      if (sanitizedCep.length !== 8) return;
+  
+  
+      fetch(`https://viacep.com.br/ws/${sanitizedCep}/json/`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.erro) {
+            console.warn("CEP não encontrado");
+            return;
+          }
+  
+          const { logradouro, localidade, bairro, estado } = data;
+          setBairro(bairro)
+          setRua(logradouro)
+          setEstado(estado)
+          setLocalidade(localidade)
+  
+        })
+    }, [cep]);
+    return (
+      <>
+        <br /><br /><br /><br />
+        <h1>LOCALIZADOR DE CEP</h1>
+        <input type="text" placeholder="Insira o CEP" onChange={handleCep} /><br></br>
+        <strong className='bairrinho'>BAIRRO:</strong>  {bairro} <br></br>  <strong>RUA:</strong>  {rua}<br></br>  <strong>ESTADO:</strong>  {estado} <br></br> <strong>CIDADE:</strong>  {localidade}
+  
+      </>
+    );
+  }
+  
+
+export default App;
---
+++ src/Contato.jsx
@@ -0,0 +1,51 @@
+
+import { useState, useEffect } from "react";
+import { Menu } from "./components/menu";
+
+
+function Contact() {
+  const [cep, setCep] = useState("80510-070");
+  const [bairro, setBairro] = useState("");
+  const [rua, setRua] = useState("");
+  const [estado, setEstado] = useState("");
+  const [localidade, setLocalidade] = useState("");
+
+
+  function handleCep(e) {
+    setCep(e.target.value);
+  }
+
+  useEffect(() => {
+    const sanitizedCep = cep.replace(/\D/g, ""); // remove qualquer caractere não numérico
+
+    if (sanitizedCep.length !== 8) return;
+
+
+    fetch(`https://viacep.com.br/ws/${sanitizedCep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.erro) {
+          console.warn("CEP não encontrado");
+          return;
+        }
+
+        const { logradouro, localidade, bairro, estado } = data;
+        setBairro(bairro)
+        setRua(logradouro)
+        setEstado(estado)
+        setLocalidade(localidade)
+
+      })
+  }, [cep]);
+  return (
+    <>
+      <br /><br /><br /><br />
+      <h1>CONTATO</h1>
+      <input type="text" placeholder="Insira o CEP" onChange={handleCep} /><br></br>
+      <strong>BAIRRO:</strong>  {bairro} <br></br>  <strong>RUA:</strong>  {rua}<br></br>  <strong>ESTADO:</strong>  {estado} <br></br> <strong>CIDADE:</strong>  {localidade}
+
+    </>
+  );
+}
+
+export default Contact;
